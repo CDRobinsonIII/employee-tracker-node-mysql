2c01696ae8af803d55597002aafd755b86486d8b
Charles Robinson <the.real.robinsons@gmail.com>
2021-01-01 07:50:50 -0800

    Updated connection.js and created database employees_DB in the schema.sql file.

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
+var mysql = require("mysql");
+
 var connection = mysql.createConnection ({
 
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
-    database: "employee_DB"
+    password: "Coding4moi!",
+    database: "employees_DB"
 
 });
 
